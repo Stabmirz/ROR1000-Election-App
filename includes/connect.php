03a45ea47088d2b2b09df9a6ad601592a1f83a37
Mirza <mirza@Mirzas-MacBook-Pro.local>
2019-02-08 20:59:24 -0500

    updated

--- includes/connect.php
+++ includes/connect.php
@@ -4,6 +4,6 @@
 session_start();
 
 // connect to the database
-$conn = new mysqli("localhost", "polls", "Password", "polls");
+$conn = new mysqli("localhost", "root", "", "polls");
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
